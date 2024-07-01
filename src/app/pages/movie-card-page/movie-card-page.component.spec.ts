bb151734cf257de9a8cb55fc23d99ca100cf2fee
Arsen <SuchyanArs@gmail.com>
2024-07-01 22:37:27 +0300

    Обнулення роботи, починаємо заново

---
+++ src/app/pages/movie-card-page/movie-card-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovieCardPageComponent } from './movie-card-page.component';
+
+describe('MovieCardPageComponent', () => {
+  let component: MovieCardPageComponent;
+  let fixture: ComponentFixture<MovieCardPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MovieCardPageComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MovieCardPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
